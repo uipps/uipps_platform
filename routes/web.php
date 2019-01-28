efa30f0d36db95f54537206e7117981480f59a47
red_boxer <red_boxer@sohu.com>
2019-01-28 23:16:19 +0800

    缓存、队列queue、Session等改用redis； 路由都加上名字name

--- routes/web.php
+++ routes/web.php
@@ -19,42 +19,42 @@ Auth::routes();
 
 Route::group(['namespace' => 'admin'], function ($router) {
     Route::get('/home', 'HomeController@index')->name('home');
-    $router->get('mainpage','AdminController@mainpage');
+    $router->get('mainpage','AdminController@mainpage')->name('mainpage');
 
 });
 
 Route::group(['namespace' => 'Hostbackend', 'prefix' => 'host'], function ($router) {
-    $router->get('list','HostController@list');
-    $router->post('edit','HostController@edit');
-    $router->post('add', 'HostController@add');
+    $router->get('list','HostController@list')->name('hostlist');
+    $router->post('edit','HostController@edit')->name('hostedit');
+    $router->post('add', 'HostController@add')->name('hostadd');
 });
 
 Route::group(['namespace' => 'Project', 'prefix' => 'project'], function ($router) {
-    $router->get('list','ProjectController@list');
-    $router->post('edit','ProjectController@edit');
-    $router->post('add', 'ProjectController@add');
+    $router->get('list','ProjectController@list')->name('projectlist');
+    $router->post('edit','ProjectController@edit')->name('projectedit');
+    $router->post('add', 'ProjectController@add')->name('projectadd');
 });
 
 Route::group(['namespace' => 'Template', 'prefix' => 'template'], function ($router) {
-    $router->get('list','TemplateController@list');
-    $router->post('edit','TemplateController@edit');
-    $router->post('add', 'TemplateController@add');
+    $router->get('list','TemplateController@list')->name('templatelist');
+    $router->post('edit','TemplateController@edit')->name('templateedit');
+    $router->post('add', 'TemplateController@add')->name('templateadd');
 });
 
 Route::group(['namespace' => 'Tempdef', 'prefix' => 'tempdef'], function ($router) {
-    $router->get('list','TempdefController@list');
-    $router->post('edit','TempdefController@edit');
-    $router->post('add', 'TempdefController@add');
+    $router->get('list','TempdefController@list')->name('tempdeflist');
+    $router->post('edit','TempdefController@edit')->name('tempdefedit');
+    $router->post('add', 'TempdefController@add')->name('tempdefadd');
 });
 
 Route::group(['namespace' => 'Document', 'prefix' => 'document'], function ($router) {
-    $router->get('list','DocumentController@list');
-    $router->post('edit','DocumentController@edit');
-    $router->post('add', 'DocumentController@add');
+    $router->get('list','DocumentController@list')->name('documentlist');
+    $router->post('edit','DocumentController@edit')->name('documentedit');
+    $router->post('add', 'DocumentController@add')->name('documentadd');
 });
 
 Route::group(['namespace' => 'Schedule', 'prefix' => 'schedule'], function ($router) {
-    $router->get('list','ScheduleController@list');
-    $router->post('edit','ScheduleController@edit');
-    $router->post('add', 'ScheduleController@add');
+    $router->get('list','ScheduleController@list')->name('schedulelist');
+    $router->post('edit','ScheduleController@edit')->name('scheduleedit');
+    $router->post('add', 'ScheduleController@add')->name('scheduleadd');
 });
