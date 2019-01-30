33f8d0575f3361cdcdd70656e4b5bbdfa9339ac7
red_boxer <red_boxer@sohu.com>
2019-01-30 16:48:25 +0800

    blade模板的contentTags用以前的，这样改动最小。执行 php artisan crontab replaceBladeContentTags 即可

--- app/Console/Commands/CrontabCommand.php
+++ app/Console/Commands/CrontabCommand.php
@@ -32,6 +32,36 @@ class CrontabCommand extends Command
         return true;
     }
 
+    private function replaceBladeContentTags() {
+        $l_path =  realpath(dirname(dirname(dirname(__DIR__))));
+        $l_file = 'vendor/laravel/framework/src/Illuminate/View/Compilers/BladeCompiler.php';
+        if (!file_exists($l_path . '/' .$l_file)) {
+            echo 'file not found: ' . $l_file;
+            return 0;
+        }
+        $content = file_get_contents($l_path . '/' .$l_file);
+
+        // 替换其中的ContentTags,
+        $str = "//protected \$contentTags = ['{{', '}}'];";
+        if (false !== strpos($content, $str)) {
+            echo 'already replace!';
+            return 1;
+        }
+        $str_from = "protected \$contentTags = ['{{', '}}'];";
+        if (false === strpos($content, $str_from)) {
+            echo 'something wrong, not found: ' . $str_from;
+            return 0;
+        }
+        // 备份文件
+        @file_put_contents($l_path . '/' .$l_file . '.back.php', $content, FILE_APPEND);
+
+        $replace_to = "//protected \$contentTags = ['{{', '}}'];\r\n    protected \$contentTags = ['<!--{', '}-->'];";
+        $content = str_replace($str_from, $replace_to, $content);
+        echo $content;
+        file_put_contents($l_path . '/' .$l_file, $content); // 覆盖原文件
+        return 3;
+    }
+
 
     // 设置参数接口
     protected function getArguments()
