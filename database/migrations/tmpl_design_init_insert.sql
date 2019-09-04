f91dc6d8f9e1704b5cd4fdea802f4f6af4854a1e
tom-cf <red_boxer@sohu.com>
2019-09-04 21:43:59 +0800

    当子查询有多条记录会报错，因此=改成IN： SQLSTATE[21000]: Cardinality violation: 1242 Subquery returns more than 1 row (SQL: update `field_def` set `list_order`=\'10\' where `name_eng`=\'id\' and `t_id` = (select id from table_def where name_eng=\'tmpl_design\'); )'

--- database/migrations/tmpl_design_init_insert.sql
+++ database/migrations/tmpl_design_init_insert.sql
@@ -4,19 +4,19 @@
 -- update  field_def set `f_type`='Form::DB_Select', `arithmetic`='[query]
 -- sql=select CONCAT(name_eng,"-",name_cn),id from table_def where `status_`=''use'' order by list_order' where name_eng='tbl_id' and t_id = (select id from table_def where name_eng='tmpl_design')
 
-update `field_def` set `list_order`='10' where `name_eng`='id' and `t_id` = (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='10' where `name_eng`='id' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
 update `field_def` set `list_order`='15',`f_type`='Form::DB_Select', `arithmetic`='[query]
-sql=select CONCAT(name_eng,"-",name_cn),id from table_def where `status_`=''use'' order by list_order' where `name_eng`='tbl_id' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='20' where `name_eng`='creator' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='21' where `name_eng`='createdate' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='22' where `name_eng`='createtime' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='35' where `name_eng`='mender' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='36' where `name_eng`='menddate' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='37' where `name_eng`='mendtime' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='40' where `name_eng`='content_type' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='100' where `name_eng`='if_publish' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='110' where `name_eng`='default_html' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='120' where `name_eng`='default_url' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='130' where `name_eng`='tmpl_expr' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='140' where `name_eng`='status_' and `t_id` = (select id from table_def where name_eng='tmpl_design');
-update `field_def` set `list_order`='1001' where `name_eng`='description' and `t_id` = (select id from table_def where name_eng='tmpl_design');
+sql=select CONCAT(name_eng,"-",name_cn),id from table_def where `status_`=''use'' order by list_order' where `name_eng`='tbl_id' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='20' where `name_eng`='creator' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='21' where `name_eng`='createdate' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='22' where `name_eng`='createtime' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='35' where `name_eng`='mender' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='36' where `name_eng`='menddate' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='37' where `name_eng`='mendtime' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='40' where `name_eng`='content_type' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='100' where `name_eng`='if_publish' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='110' where `name_eng`='default_html' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='120' where `name_eng`='default_url' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='130' where `name_eng`='tmpl_expr' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='140' where `name_eng`='status_' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
+update `field_def` set `list_order`='1001' where `name_eng`='description' and `t_id` IN (select id from table_def where name_eng='tmpl_design');
